3e286fc843853d92826889e32986347cb33ed8d5
Rafael Godinho <rafael.godinho@dextra-sw.com>
2019-10-08 10:45:50 -0300

    - Add MainActivity

---
+++ app/src/main/java/com/hallel/hallelsomevida/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hallel.hallelsomevida
+
+import android.os.Bundle
+import com.hallel.core_ui.base.BaseActivity
+
+class MainActivity: BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
